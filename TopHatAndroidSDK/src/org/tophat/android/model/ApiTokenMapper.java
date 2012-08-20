5987730172327e47c281bc69ea46b9ac0dc39be2
Kevin Bluett <kevin.bluett@gmail.com>
2012-08-20 15:25:44 +0200

    Made it possible to retrieve a valid Anonymous API Token from the API.

--- TopHatAndroidSDK/src/org/tophat/android/model/ApiTokenMapper.java
+++ TopHatAndroidSDK/src/org/tophat/android/model/ApiTokenMapper.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.tophat.android.exceptions.HttpException;
 import org.tophat.android.mapping.ApiToken;
+import org.tophat.android.mapping.Player;
 import org.tophat.android.networking.ApiCommunicator;
 
 public class ApiTokenMapper extends Mapper 
@@ -14,6 +15,13 @@ public class ApiTokenMapper extends Mapper
 		super(apic);
 	}
 	
+	/**
+	 * This method returns a valid Api
+	 * @param username
+	 * @param password
+	 * @return
+	 * @throws HttpException
+	 */
 	public ApiToken getApiToken(String username, String password) throws HttpException
 	{
 		HashMap<String, String> map = new HashMap<String, String>();
@@ -21,6 +29,23 @@ public class ApiTokenMapper extends Mapper
 		map.put("email", username);
 		map.put("password", password);
 		
-		return new ApiToken(this.getApiCommunicator().post("apitokens/", map));
+		return new ApiToken(this.getApiCommunicator().post(ApiToken.API_URI+"/", (Map)map));
+	}
+	
+	/**
+	 * This method posts an empty JSON data packet to the API Url, which returns a valid Anonymous API token 
+	 * @return
+	 * @throws HttpException
+	 * @throws Unimplemented This method may throw an unimplemented 
+	 */
+	public ApiToken getAnonymousToken() throws HttpException
+	{
+		return new ApiToken(this.getApiCommunicator().post(ApiToken.API_URI+"/", (Map)new HashMap<String, Object>()));
+	}
+	
+	@Override
+	protected ApiToken creator(Map<String, Object> data)
+	{
+		return new ApiToken(data);
 	}
 }
