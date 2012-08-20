10498588f8114b938ceb9c880997bcdeae73e11e
Kevin Bluett <kevin.bluett@gmail.com>
2012-08-20 15:27:05 +0200

    Provided the basic CREATOR override required for standard object creation from passed data

--- TopHatAndroidSDK/src/org/tophat/android/model/PlayerMapper.java
+++ TopHatAndroidSDK/src/org/tophat/android/model/PlayerMapper.java
@@ -1,12 +1,21 @@
 package org.tophat.android.model;
 
+import java.util.Map;
+
+import org.tophat.android.mapping.Player;
 import org.tophat.android.networking.ApiCommunicator;
 
 public class PlayerMapper extends Mapper {
 
 	public PlayerMapper(ApiCommunicator apic) {
 		super(apic);
-		// TODO Auto-generated constructor stub
 	}
+	
+	@Override
+	protected Player creator(Map<String, Object> data)
+	{
+		return new Player(data);
+	}
+
 
 }
